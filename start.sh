80d2725081d4ae05e97f7795562ace68ac71198b
xneh29e4eqku <99010871+xneh29e4eqku@users.noreply.github.com>
2023-05-26 09:13:16 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
+export UUID=2a3f96c6-3b15-46e5-a6a2-3adae086d8f0
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=joyfreak.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
